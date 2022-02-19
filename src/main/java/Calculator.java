01196fd7ef62c83bb7881b656f0bc93d2e888079
cjbeacon <calebj2016@gmail.com>
2022-02-18 22:16:43 -0700

    fib fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,16 +39,19 @@ class Calculator {
     .
     etc
      */
-    static int n1=0,n2=1,n3=0;
     int fibonacciNumberFinder(int n){
-        if(n>0){
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-/*            if (n == 1) {
-                return n3;
-            }*/
-            return fibonacciNumberFinder(n-1);
+        int n1=0,n2=1,n3=0;
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1) {
+            return n;
+        }
+        for(int i=2;i<=n;++i)//loop starts from 2 because 0 and 1 are already printed
+        {
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
         }
         return n3;
     }
